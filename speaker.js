15c4f2d4549d2371513ff4acf2ce2149efd1a2c8
DVLP <pawel.misiurski@gmail.com>
2015-10-20 17:49:21 +0100

    Looks like no block comments in bookmarklets allowed

--- speaker.js
+++ speaker.js
@@ -1,1 +1,61 @@
-javascript: 'READ SELECTED'; (function () {  'use strict';  var voices = window.speechSynthesis.getVoices();  var sayit = function () {   var msg = new SpeechSynthesisUtterance();   msg.voice = voices[2];   msg.voiceURI = 'native';   msg.volume = 1;   msg.rate = 1.15;   msg.pitch = 0.7;   msg.lang = 'en-GB';   msg.onstart = function (event) {    console.log('started');   };   msg.onend = function (event) {    console.log('Finished in ' + event.elapsedTime + ' seconds.');   };   msg.onerror = function (event) {    console.log('Errored ' + event);   };   msg.onpause = function (event) {    console.log('paused ' + event);   };   msg.onboundary = function (event) {    console.log('onboundary ' + event);   };   return msg;  };  var speekResponse = function (sel) {   var text = addPauses(sel).textContent;   window.speechSynthesis.cancel();   var sentences = text.split('.');   for (var i = 0; i < sentences.length; i++) {    var toSay = sayit();    toSay.text = sentences[i];    window.speechSynthesis.speak(toSay);   }  };  function addPauses(sel) {   var el = document.createElement('div');   el.appendChild(sel.getRangeAt(0).cloneContents()); /* see the output for debugging document.body.appendChild(el); */   for (var i = 0; i < 10; i++) {    addMissingDots(el);   }   return el;  } /* requires multiple iterations. Can be replaced with a smarter approach */  function addMissingDots(el) {   var els = el.querySelectorAll('p, li, ol, h1, h2, h3, h4, h5, h6, h7, div');   for (var i = 0; i < els.length; i++) {    var cont = els[i].textContent;    if (cont.length && cont.substr(cont.length - 1) !== '.') {     els[i].innerHTML += '.';    }   }   return el;  }  speekResponse(window.getSelection()); })();
+javascript:'READ SELECTED';
+(function () {
+    'use strict';
+    var voices = window.speechSynthesis.getVoices();
+    var sayit = function () {
+        var msg = new SpeechSynthesisUtterance();
+        msg.voice = voices[2];
+        msg.voiceURI = 'native';
+        msg.volume = 1;
+        msg.rate = 1.15;
+        msg.pitch = 0.7;
+        msg.lang = 'en-GB';
+        msg.onstart = function (event) {
+            console.log('started');
+        };
+        msg.onend = function (event) {
+            console.log('Finished in ' + event.elapsedTime + ' seconds.');
+        };
+        msg.onerror = function (event) {
+            console.log('Errored ' + event);
+        };
+        msg.onpause = function (event) {
+            console.log('paused ' + event);
+        };
+        msg.onboundary = function (event) {
+            console.log('onboundary ' + event);
+        };
+        return msg;
+    };
+    var speekResponse = function (sel) {
+        var text = addPauses(sel).textContent;
+        window.speechSynthesis.cancel();
+        var sentences = text.split('.');
+        for (var i = 0; i < sentences.length; i++) {
+            var toSay = sayit();
+            toSay.text = sentences[i];
+            window.speechSynthesis.speak(toSay);
+        }
+    };
+
+    function addPauses(sel) {
+        var el = document.createElement('div');
+        el.appendChild(sel.getRangeAt(0).cloneContents());
+        for (var i = 0; i < 10; i++) {
+            addMissingDots(el);
+        }
+        return el;
+    }
+
+    function addMissingDots(el) {
+        var els = el.querySelectorAll('p, li, ol, h1, h2, h3, h4, h5, h6, h7, div');
+        for (var i = 0; i < els.length; i++) {
+            var cont = els[i].textContent;
+            if (cont.length && cont.substr(cont.length - 1) !== '.') {
+                els[i].innerHTML += '.';
+            }
+        }
+        return el;
+    }
+    speekResponse(window.getSelection());
+})();
